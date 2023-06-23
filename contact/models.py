53882df7f98d3992a742e848eb3dcd28df6fcdbb
Luan <Luan.ab2002@yahoo.com.br>
2023-06-23 20:41:22 -0300

    Django Models - Creating the initial Contact model and registering

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField(max_length=240, blank=True)
+    description = models.TextField(blank=True)
+    created_contact = models.DateTimeField(default=timezone.now)
