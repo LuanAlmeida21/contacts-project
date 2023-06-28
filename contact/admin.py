57145375ffec827392cbb327157ed6dad9260e1e
Luan <Luan.ab2002@yahoo.com.br>
2023-06-28 20:31:01 -0300

    Made Query for views, config of the style e created HTML template

--- contact/admin.py
+++ contact/admin.py
@@ -6,13 +6,13 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'phone']
+    list_display = ['id', 'first_name', 'last_name', 'phone', 'show']
     search_fields = ['id', 'first_name']
     ordering = ['-id']
     list_per_page = 10
     list_max_show_all = 240
     list_display_links = ['id', 'phone']
-    list_editable = ['first_name']
+    list_editable = ['first_name', 'show']
 
 
 @admin.register(models.Category)
