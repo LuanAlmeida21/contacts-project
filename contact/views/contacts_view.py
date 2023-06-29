252e6ff3976c45f79c23e6bf0a8ac88dbacf26f4
Luan <Luan.ab2002@yahoo.com.br>
2023-06-29 20:26:44 -0300

    creating partials

--- contact/views/contacts_view.py
+++ contact/views/contacts_view.py
@@ -8,7 +8,8 @@ from contact.models import Contact
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Default'
     }
 
     return render(
@@ -22,8 +23,11 @@ def contact(request, contact_id):
 
     single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id))
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name}'
+
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
