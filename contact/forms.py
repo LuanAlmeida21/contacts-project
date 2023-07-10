b89245daf84421e9b31abc8e8fb506cd098735b6
Luan <Luan.ab2002@yahoo.com.br>
2023-07-10 20:37:34 -0300

    Create Update/Delete functions and added more detail to the templates

--- contact/forms.py
+++ contact/forms.py
@@ -1,16 +1,13 @@
+# flake8: noqa
 from django import forms
-from django.core.exceptions import ValidationError
 
 from contact.models import Contact
 
+# from django.core.exceptions import ValidationError
+
 
 class ContactForm(forms.ModelForm):
+
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name', 'phone')
-
-    def clean(self):
-        # cleaned_data = self.cleaned_data
-
-        self.add_error('first_name', ValidationError('Error Message'))
-        return super().clean()
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category')
--- contact/views/create_form.py
+++ contact/views/create_form.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
 from contact.forms import ContactForm
+from contact.models import Contact
 
 
 def create(request):
+    title_create = 'Create'
+    form_action = reverse('contact:create')
 
     if request.method == 'POST':
+
+        form = ContactForm(request.POST)
+
         context = {
-            'form': ContactForm(request.POST)
+            'form': form,
+            'form_action': form_action,
+            'title': title_create
         }
+
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.id)
 
         return render(
             request,
@@ -17,7 +30,9 @@ def create(request):
         )
 
     context = {
-        'form': ContactForm()
+        'form': ContactForm(),
+        'form_action': form_action,
+        'title': title_create
     }
 
     return render(
@@ -25,3 +40,49 @@ def create(request):
         'contact/create.html',
         context
     )
+
+
+def update(request, contact_id):
+
+    title_update = 'Update'
+    contact = get_object_or_404(Contact, id=contact_id, show=True)
+    form_action = reverse('contact:update', args=(contact_id,))
+
+    if request.method == 'POST':
+
+        form = ContactForm(request.POST, instance=contact)
+
+        context = {
+            'form': form,
+            'form_action': form_action,
+            'title': title_update,
+        }
+
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.id)
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+
+    context = {
+        'form': ContactForm(instance=contact),
+        'form_action': form_action,
+        'title': title_update,
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context
+    )
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id, show=True)
+
+    contact.delete()
+    return redirect('contact:index')
