4cfe9682c4a937696a3e9460ae73dbb9953eeab9
Luan <Luan.ab2002@yahoo.com.br>
2023-07-11 21:14:01 -0300

    create field for profile

--- contact/forms.py
+++ contact/forms.py
@@ -8,6 +8,14 @@ from contact.models import Contact
 
 class ContactForm(forms.ModelForm):
 
+    picture = forms.ImageField(widget=forms.FileInput(
+        attrs={
+            'accept': 'image/*',
+        }
+    )
+    )
+
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category')
+        fields = ('first_name', 'last_name', 'phone', 'email',
+                  'description', 'category', 'picture')
